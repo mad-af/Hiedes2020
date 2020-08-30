118c4245139d02c49bbccf7e03b87e307479c60f
Mochamad Aufa <mochamadaufa7@gmail.com>
2020-08-30 21:37:17 +0700

    lab-change duration transition slide

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,14 @@
 let isPoint = [1, 2, 3, 0]
 function next(x) {
-  let left = 1056 * isPoint[x]
-  document.getElementById("slide").style.left = "-"+ left +"px";
-  document.getElementById("slide").style.WebkitTransition = "left 0.5s";
-  document.getElementById("slide").style.MozTransition = "left 0.5s";
+  let range = 1056 * isPoint[x]
+  document.getElementById("slide").style.left = "-"+ range +"px";
+  document.getElementById("slide").style.WebkitTransition = "left 0.7s";
+  document.getElementById("slide").style.MozTransition = "left 0.7s";
 }
 
 function prev(x) {
-  let left = 1056 * isPoint[x]
-  document.getElementById("slide").style.left = "-"+ left +"px";
-  document.getElementById("slide").style.WebkitTransition = "left 0.5s";
-  document.getElementById("slide").style.MozTransition = "left 0.5s";
+  let range = 1056 * isPoint[x]
+  document.getElementById("slide").style.left = "-"+ range +"px";
+  document.getElementById("slide").style.WebkitTransition = "left 0.7s";
+  document.getElementById("slide").style.MozTransition = "left 0.7s";
 }
